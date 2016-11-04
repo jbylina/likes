da2fc7ee1e73ee4f0be3b39a5e65013177e0d7b5
Jacek Bylina <jacek.bylina@gmail.com>
2016-11-04 16:57:19 +0100

    add spring boot application

---
+++ src/main/java/pw/rapit/likes/LikesApplication.java
@@ -0,0 +1,12 @@
+package pw.rapit.likes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LikesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LikesApplication.class, args);
+	}
+}
---
+++ src/test/java/pw/rapit/likes/LikesApplicationTests.java
@@ -0,0 +1,16 @@
+package pw.rapit.likes;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class LikesApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
