4d7455234557f322f962049c8d0a4597e040a797
mixnix <bukaniex@gmail.com>
2016-11-06 20:42:39 +0100

    removed unnecessary lines

--- src/main/java/pw/rapit/likes/FacebookFetcher.java
+++ src/main/java/pw/rapit/likes/FacebookFetcher.java
@@ -3,27 +3,24 @@ package pw.rapit.likes;
 import com.restfb.DefaultFacebookClient;
 import com.restfb.FacebookClient;
 import com.restfb.Parameter;
+import com.restfb.Version;
 import com.restfb.types.Post;
 
 public class FacebookFetcher {
 
 
     void getLikes() {
-        String accessToken = "EAACEdEose0cBAAwbZBU0gyIgSwsjPBLHakKkLfA0g5IqvIZBX95dnbApCgFdDXEBoandQeI14vrbkMYrCZAdzGlt9jCPunFaahcFDd08RuPFOjVrkJe3e9dE729kdgbzts0ZAizwXHjSQa2buaR4LyO8fZBgt4MPd4arcC7ZBrg1zzAQSWtvCa";
-
+        String accessToken = "EAAZAOUsPZBGV8BADUfh0CtigHv7KpwhIj9GL2sQ27NldSNlfeZC6QS1nZBou6T9C6lB2C3Ejm7oWZACHcyBZAvZAGvqssPRP2E5NTnRY5r5iVliDcBVTCw0yH3peKhmnavb7HVolXZCn6vmZAEbtvL2ZBmY4cfMntZCWy03VmpVKooNBfbxpFMZAZANGx";
         FacebookClient fbClient = new DefaultFacebookClient(accessToken);
 
-        Post post = fbClient.fetchObject("1019734961433937", Post.class);
+        //post id:10155273876115329           buzzfeed chichacho cubs wins         https://www.facebook.com/BuzzFeed/videos/10155273876115329/
+        Post post = fbClient.fetchObject("10155273876115329", Post.class);
         Post.Likes likes = fbClient.fetchObject(post.getId() + "/likes", Post.Likes.class,
                 Parameter.with("summary", 1), Parameter.with("limit", 0));
         long likesTotalCount = likes.getTotalCount();
 
-        Post.Comments comments = fbClient.fetchObject(post.getId() + "/comments", Post.Comments.class,
-                Parameter.with("summary", 1), Parameter.with("limit", 0));
-        long commentsTotalCount = comments.getTotalCount();
-
         System.out.println(likesTotalCount);
-        //dodałem komentarz
+
     }
 
 
